00abac5ff72689864b465ee921386cf9f6a77f72
JonasBaekbo <jonas.bryde.baekbo@live.dk>
2022-08-26 09:01:51 +0200

    First Commit

---
+++ src/test/java/azurehelloworld/AzureHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package azurehelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureHelloWorldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
